f92eecb67c4d63a2505c60c5437339e1098c333e
Jarrett Gliner <jarrett.gliner@gmail.com>
2016-05-20 17:28:57 -0700

    Switch server to prod

--- server/server.js
+++ server/server.js
@@ -2,17 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
 
-app.use(morgan('dev'));
 app.use(bodyParser.json({ limit: 100000000}));
 app.use(express.static(path.join(__dirname, '../client')));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 console.log(`listening on ${port}`);
-// start listening to requests on port 3000
 app.listen(port);
